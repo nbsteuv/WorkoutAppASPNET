b17be612ea3975608a73c411470ebd606cdacc07
Nick Steuver <nbsteuv@gmail.com>
2016-01-05 16:25:30 -0500

    Added ExerciseSet class with properties to WorkoutApp.Model #24

---
+++ WorkoutApp/WorkoutApp.Model/ExerciseSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutApp.Model
+{
+    public class ExerciseSet
+    {
+        public int ExerciseSetId { get; set; }
+        public int ExerciseSetWeight { get; set; }
+        public int ExerciseSetReps { get; set; }
+    }
+}
