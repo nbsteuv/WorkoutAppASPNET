65b42dd36c4bbab4e2a771591d7b70bc34a27853
Nick Steuver <nbsteuv@gmail.com>
2016-01-05 17:02:19 -0500

    Created Exercise, ExerciseSet, and WorkoutSession repositories and created DataContext instances for each.

---
+++ WorkoutApp/WorkoutApp.DAL/Repositories/ExerciseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutApp.DAL.Data;
+
+namespace WorkoutApp.DAL.Repositories
+{
+    public class ExerciseRepository
+    {
+        internal DataContext context;
+    }
+}
