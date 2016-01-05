63e36c4f21fb18849db2d6219816b93df73e2f78
Nick Steuver <nbsteuv@gmail.com>
2016-01-05 18:58:30 -0500

    Created repository base in DAL #24

---
+++ WorkoutApp/WorkoutApp.DAL/Repositories/RepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutApp.DAL.Data;
+
+namespace WorkoutApp.DAL.Repositories
+{
+    public abstract class RepositoryBase<TEntity> where TEntity : class
+    {
+        //I'm not completely clear here, so I'll have some questions if I don't find the answers in my reading by our first code review.
+
+        internal DataContext context;
+        internal DbSet<TEntity> dbSet;
+
+        public RepositoryBase(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+    }
+}
