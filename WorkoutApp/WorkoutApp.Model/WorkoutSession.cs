c29da404c15f288443d0ca0d82790d0be38bed35
Nick Steuver <nbsteuv@gmail.com>
2016-01-05 16:39:27 -0500

    Added WorkoutSession class to WorkoutApp.Model #24

---
+++ WorkoutApp/WorkoutApp.Model/WorkoutSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutApp.Model
+{
+    public class WorkoutSession
+    {
+        public int WorkoutSessionId { get; set; }
+        public DateTime SessionDate { get; set; }
+        public ICollection<Exercise> SessionExerciseList { get; set; }
+
+    }
+}
