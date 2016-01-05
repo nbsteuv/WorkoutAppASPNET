74e98277423137087a0582335932b2401f37ee6d
Nick Steuver <nbsteuv@gmail.com>
2016-01-05 16:56:19 -0500

    Added DbSets for ExerciseSet, Exercise, and WorkoutSession to DataContext class #24

--- WorkoutApp/WorkoutApp.DAL/Data/DataContext.cs
+++ WorkoutApp/WorkoutApp.DAL/Data/DataContext.cs
@@ -4,10 +4,20 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkoutApp.Model;
 
 namespace WorkoutApp.DAL.Data
 {
     class DataContext : DbContext
     {
+        public DataContext() : base("DefaultConnection")
+        {
+            //Concern about this: It was in the course but not discussed.
+        }
+
+        public DbSet<ExerciseSet> ExerciseSets { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<WorkoutSession> WorkoutSessions { get; set; }
+
     }
 }
