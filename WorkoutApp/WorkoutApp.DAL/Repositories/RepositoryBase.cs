8ee1f40a541e884928cd12403d0ca36c61f38cbf
Nick Steuver <nbsteuv@gmail.com>
2016-01-12 16:05:19 -0500

    Created CreateExercise method in Repository Base #1

--- WorkoutApp/WorkoutApp.DAL/Repositories/RepositoryBase.cs
+++ WorkoutApp/WorkoutApp.DAL/Repositories/RepositoryBase.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WorkoutApp.DAL.Data;
+using WorkoutApp.Model;
 
 namespace WorkoutApp.DAL.Repositories
 {
@@ -25,6 +26,17 @@ namespace WorkoutApp.DAL.Repositories
         public virtual string Test()
         {
             return ("Repository test method for " + context + " successful.") ;
+        }
+
+        public virtual void CreateExercise(string muscleGroup, string exerciseName, int weight, int reps)
+        {
+            Exercise newExercise = new Exercise();
+            newExercise.MuscleGroup = muscleGroup;
+            newExercise.ExerciseName = exerciseName;
+            ExerciseSet newExerciseSet = new ExerciseSet();
+            newExerciseSet.ExerciseSetWeight = weight;
+            newExerciseSet.ExerciseSetReps = reps;
+            newExercise.ExerciseSetList.Add(newExerciseSet);
         }
 
     }
